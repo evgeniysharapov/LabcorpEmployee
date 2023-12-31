226695d04cac293b4827f8e6939d30dbaab693ed
Evgeniy N. Sharapov <esharapov@ainq.com>
2023-12-30 22:44:45 -0500

    basic application structure

---
+++ src/main/java/com/labcorp/employee/LabcorpEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.labcorp.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabcorpEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabcorpEmployeeApplication.class, args);
+	}
+
+}
