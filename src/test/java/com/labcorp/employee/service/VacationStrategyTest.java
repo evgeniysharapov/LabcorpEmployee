2dbcfc8153cb2a9b05671f7a482fe13d2416654b
Evgeniy N. Sharapov <esharapov@ainq.com>
2024-01-04 15:40:25 -0500

    added few more tests for vacation strategy

--- src/test/java/com/labcorp/employee/service/VacationStrategyTest.java
+++ src/test/java/com/labcorp/employee/service/VacationStrategyTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.sql.Date;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(classes = {VacationStrategy.class})
 public class VacationStrategyTest {
@@ -49,5 +50,82 @@ public class VacationStrategyTest {
 
         vacationStrategy.work(employee, 260);
         assertDoesNotThrow(() -> vacationStrategy.takeVacation(employee, 15));
+    }
+
+    @Test
+    public void testManagerEmployee_Work_WholeYear() {
+        Employee employee = Employee
+                .builder()
+                .id("1")
+                .employeeType(EmployeeType.MANAGER)
+                .firstName("John")
+                .lastName("Doe")
+                .isMale(true)
+                .dateOfBirth(Date.valueOf("1980-01-01"))
+                .build();
+
+        vacationStrategy.work(employee, 260);
+        assertDoesNotThrow(() -> vacationStrategy.takeVacation(employee, 30));
+    }
+
+    @Test
+    public void testHourlyEmployee_Work_OverYear() {
+        Employee employee = Employee
+                .builder()
+                .id("1")
+                .employeeType(EmployeeType.HOURLY)
+                .firstName("John")
+                .lastName("Doe")
+                .isMale(true)
+                .dateOfBirth(Date.valueOf("1980-01-01"))
+                .build();
+
+        assertThrows(IllegalArgumentException.class, () -> vacationStrategy.work(employee, 261));
+    }
+
+    @Test
+    public void testSalariedEmployee_Work_OverYear() {
+        Employee employee = Employee
+                .builder()
+                .id("1")
+                .employeeType(EmployeeType.SALARIED)
+                .firstName("John")
+                .lastName("Doe")
+                .isMale(true)
+                .dateOfBirth(Date.valueOf("1980-01-01"))
+                .build();
+
+        assertThrows(IllegalArgumentException.class, () -> vacationStrategy.work(employee, 261));
+    }
+
+    @Test
+    public void testManagerEmployee_Work_OverYear() {
+        Employee employee = Employee
+                .builder()
+                .id("1")
+                .employeeType(EmployeeType.MANAGER)
+                .firstName("John")
+                .lastName("Doe")
+                .isMale(true)
+                .dateOfBirth(Date.valueOf("1980-01-01"))
+                .build();
+
+        assertThrows(IllegalArgumentException.class, () -> vacationStrategy.work(employee, 261));
+    }
+
+    @Test
+    public void testHourlyEmployee_NotEnoughVacation() {
+        Employee employee = Employee
+                .builder()
+                .id("1")
+                .employeeType(EmployeeType.HOURLY)
+                .firstName("John")
+                .lastName("Doe")
+                .isMale(true)
+                .dateOfBirth(Date.valueOf("1980-01-01"))
+                .build();
+
+        vacationStrategy.work(employee, 2);
+        assertThrows(IllegalArgumentException.class, () -> vacationStrategy.takeVacation(employee, 1));
     }
 }
